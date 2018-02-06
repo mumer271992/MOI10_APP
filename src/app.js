5291461b85429b32b3e1060efd6ab81cb6503262
Muhammad Umer <mumer271992@gmail.com>
2018-02-06 21:54:43 +0500

    Production

--- src/app.js
+++ src/app.js
@@ -6,10 +6,9 @@ import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-// import { firebase } from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
